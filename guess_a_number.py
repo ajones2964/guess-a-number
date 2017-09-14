c04427aa723102b6588feaa16331c0139eb32fb2
Jones <ajones2964@greenville.k12.sc.us>
2017-09-14 09:22:34 -0400

    added endings and limited number of tries

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,13 +1,18 @@
 import random
+
+# config
 low = 1
 high = 100
+limit = 10
 
+# start game
 rand = random.randint(low,high)
 print("I'm thinking of a number from 1 to 100.");
 
 guess = -1
+tries = 0
 
-while guess != rand:
+while guess != rand and tries < 10:
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -15,7 +20,11 @@ while guess != rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-    else:
-        print("You got it!")
 
-print("Game over")
+    tries += 1
+
+# Ending
+if guess == rand:
+    print("You win")
+else:
+    print("You're dumb, the number i was thinking of is " + str(rand) + ".")
